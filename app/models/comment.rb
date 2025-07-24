cbb032a73bc00e34afc978aed1a9eaece781c890
hillarykibetkirui <hillarykibetkirui@gmail.com>
2025-07-24 22:31:23 +0200

    Submission 2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
-class Comment < ApplicationRecord
+# == Schema Information
+# 
+# Table name: comments
+# id         :bigint           not null, primary key
+# body       :text
+# created_at :datetime         not null
+# updated_at :datetime         not null 
+# 
+
+class Comment < ActiveRecord::Base
+  belongs_to :user
   belongs_to :post
+  validates_presence_of :post_id
+  validates_presence_of :body
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
-class Post < ApplicationRecord
+# == Schema Information
+# Table name: posts
+# id         :bigint           not null, primary key
+#  title      :string
+# body       :text
+# created_at :datetime         not null
+# updated_at :datetime         not null
+
+
+class Post < ActiveRecord::Base
+    has_many :comments, dependent: :destroy
+    belongs_to :user
+    validates_presence_of :title
+    validates_presence_of :body
 end
